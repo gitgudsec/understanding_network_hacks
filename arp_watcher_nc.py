7804f7e7d578f3266a7419b2774ac0e39ed5e7f9
Faan Ross <moi@faanross.com>
2023-03-11 13:53:36 -0500

    arp watcher

--- arp_watcher_nc.py
+++ arp_watcher_nc.py
@@ -6,3 +6,7 @@ import sys
 
 arp_watcher_db_file = "/var/ cache/arp -watcher.db"
 ip_mac = {}
+
+# Save ARP table on shutdown
+def sig_int_handler( signum , frame):
+    print("Got SIGINT. Saving ARP database...")
